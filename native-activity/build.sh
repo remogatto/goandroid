ac58ae6e033a723417cbae61a98e3981a0b09695
remogatto <andrea.fazzi@alcacoop.it>
2013-10-27 09:46:58 +0100

    Blank GOROOT

--- native-activity/build.sh
+++ native-activity/build.sh
@@ -5,6 +5,6 @@ set -e
 mkdir -p android/libs/armeabi-v7a
 mkdir -p android/obj/local/armeabi-v7a
 CC="$NDK_ROOT/bin/arm-linux-androideabi-gcc"
-CC=$CC GOPATH="`pwd`:$GOPATH" GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 ../go/bin/go install $GOFLAGS -v -ldflags="-android -shared -extld $CC -extldflags '-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16'" -tags android nativeactivity
+CC=$CC GOPATH="`pwd`:$GOPATH" GOROOT="" GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 ../go/bin/go install $GOFLAGS -v -ldflags="-android -shared -extld $CC -extldflags '-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16'" -tags android nativeactivity
 cp bin/linux_arm/nativeactivity android/libs/armeabi-v7a/libnativeactivity.so
 cp bin/linux_arm/nativeactivity android/obj/local/armeabi-v7a/libnativeactivity.so
